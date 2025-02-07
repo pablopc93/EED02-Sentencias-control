455bebcdbdb8150ecba71c6d0c8d137ecee00f81
Pablo Pérez Cantó <pablo0903pc@gmail.com>
2025-02-07 13:27:18 +0100

    Primera vista

---
+++ sc.py
@@ -0,0 +1,19 @@
+
+a=50
+
+if a%2==0:
+    print(f"El valor {a} es par")
+else: 
+    print(f"El valor {a} es impar")
+
+if a>50:
+    print(f"El valor {a} es mayor a 50")
+elif a<50:
+    print(f"El valor {a} es menor de 50")
+else:
+    print(f"El valor {a} es igual a 50")
+
+num = input("Introduce un numero: ")
+print("Valor introducido",num)
+
+
